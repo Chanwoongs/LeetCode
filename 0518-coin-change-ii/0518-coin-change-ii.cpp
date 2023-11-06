3716ff0e790a0946b4803b12f4ebeb1543148ea1
Chanwoong Bang <tkfkdcksdnd@daum.net>
2023-11-07 01:10:47 +0900

    Time: 20 ms (53.98%), Space: 13.9 MB (67.46%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int dp[304][5004];
+        int n = coins.size();
+        
+        memset(dp, 0, sizeof(dp));
+        
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= amount; j++)
+            {
+                if (i == 0) 
+                {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                else if (j == 0) 
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                if (coins[i - 1] <= j)
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][amount];
+    }
+};
