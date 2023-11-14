2dc534c6a5c66a84c333f0b319a21cd02cc42a86
Chanwoong Bang <tkfkdcksdnd@daum.net>
2023-11-14 16:27:29 +0900

    Time: 335 ms (72.41%), Space: 90.1 MB (83.86%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -9,11 +9,6 @@ public:
         
         sort(intervals.begin(), intervals.end(), [](auto& a, auto& b){return a[1] < b[1];});
         
-        for (auto& ele : intervals)
-        {
-            cout << ele[0] << ele[1] << '\n';
-        }
-        
         vector<int> cur = intervals[0];
         res = 1;
         for (int i = 1; i < intervals.size(); i++)
